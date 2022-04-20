ad84f11334e1c41cb386c43e2cac570a48be4601
Vermesheleva <90042021+Vermesheleva@users.noreply.github.com>
2022-04-20 19:51:16 +0300

    Update RevDSU.java

--- Структуры данных/0.2 Разрушение дорог/RevDSU.java
+++ Структуры данных/0.2 Разрушение дорог/RevDSU.java
@@ -13,7 +13,22 @@ public class RevDSU {
     private int ans;
     private int len1;
     private int len2;
-    private int[][] requests;
+    private int pos;
+    private int[][] roads;
+    private int[] requests;
+
+    public RevDSU(int n, int k, int q) {
+        this.n = n;
+        this.k = k;
+        this.q = q;
+        arr = new int[n + 1];
+        roads = new int[3][];
+        roads[0] = new int[k + 1];
+        roads[1] = new int[k + 1];
+        requests = new int[q];
+        ans = n;
+        Arrays.fill(arr, -1);
+    }
 
     int findSet(int elem, int len){
         if(arr[elem] < 0) {
@@ -38,58 +53,88 @@ public class RevDSU {
             arr[parent] += temp;
             ans--;
         }
+
         len1 = 0;
         len2 = 0;
     }
 
+    public void restoreRoads(BufferedReader br) throws IOException{
+        roads[2] = new int[k + 1];
+        for(int i = 0; i < q; i++){
+            requests[i] = Integer.parseInt(br.readLine());
+            roads[2][requests[i]] = 1;
+        }
+        int e1, e2;
+        for(int i = 1; i < k + 1; i++){
+            if(roads[2][i] != 1){
+                e1 = roads[0][i];
+                e2 = roads[1][i];
+                union(e1, e2);
+            }
+        }
+    }
 
-    public void solution(String filename) throws IOException {
-        FileWriter fw = new FileWriter("output.txt");
-        BufferedReader br = new BufferedReader(new FileReader(filename));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        q = Integer.parseInt(st.nextToken());
-
-        arr = new int[n + 1];
-        requests = new int[k + 1][k + 1];
-        ans = n;
-        Arrays.fill(arr, -1);
+    public void solution(BufferedReader br) throws IOException {
+        StringTokenizer st;
 
         for(int i = 1; i < k + 1; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            requests[0][i] = Integer.parseInt(st.nextToken());
-            requests[1][i] = Integer.parseInt(st.nextToken());
+            roads[0][i] = Integer.parseInt(st.nextToken());
+            roads[1][i] = Integer.parseInt(st.nextToken());
+        }
+
+        if(q < k){
+           restoreRoads(br);
+           if(ans == 1){
+               pos = q;
+               return;
+           }
+        }
+        else {
+        for(int i = 0; i < q; i++){
+                requests[i] = Integer.parseInt(br.readLine());
+            }
         }
 
         int e1, e2, ind;
 
-        for(int i = 0; i < q; i++){
-            if(ans == 1){
-                fw.write(Integer.toString(0));
-            }
-
-            else {
-                ind = Integer.parseInt(br.readLine());
-                e1 = requests[0][ind];
-                e2 = requests[1][ind];
+        for(int i = q - 1; i >= 0; i--){
+            if(ans > 1) {
+                ind = requests[i];
+                e1 = roads[0][ind];
+                e2 = roads[1][ind];
 
                 union(e1, e2);
 
-                if(ans > 1){
-                    fw.write(Integer.toString(1));
-                }
-                else{
-                    fw.write(Integer.toString(0));
+                if(ans == 1){
+                   pos = i;
+                   break;
                 }
             }
         }
-        fw.close();
 
     }
+
+    public void writeAnswer() throws IOException{
+        FileWriter fw = new FileWriter("output.txt");
+        for (int i = 0; i < pos; i++){
+            fw.write(Integer.toString(1));
+        }
+        for (int i = pos; i < q; i++){
+            fw.write(Integer.toString(0));
+        }
+        fw.close();
+    }
     public static void main(String[] args) throws IOException{
-        RevDSU dsu = new RevDSU();
-        dsu.solution("input.txt");
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        RevDSU dsu = new RevDSU(n, k, q);
+        dsu.solution(br);
+        dsu.writeAnswer();
     }
 }
